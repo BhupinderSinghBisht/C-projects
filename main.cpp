3bfc5a001df2b1f5006e076ec4f6f479746896d6
Bhuwan Bisht <37290289+BhupinderSinghBisht@users.noreply.github.com>
2018-03-13 00:03:02 +0530

    Ascii_Values

--- main.cpp
+++ main.cpp
@@ -4,45 +4,12 @@ using namespace std;
 
 int main()
 {
-    string bhu("What a wonderfull day it is");// put your paragraph in here
-    string bhup("it");
-    size_t found;
+     char a;
+     cout<<"please enter an alphabet you wanna find ascii value of :";
+     cin>>a;
+
+     cout<<"ascii  will be :"<<(int)a;
 
 
- found = bhu.find("day" );
-    if (found  != string::npos ){
-    cout<<"first word found   at  ";
-    cout<<found<<endl;
-    }
-     found = bhu.find(bhup);
-    if(found != string::npos)
-    {
-        cout<<"second  world found at: " ;
-        cout<<found<< endl;
-    }
-
-    else{
-
-    cout<<"nothing here"<< endl<< endl;
-    }
-
-    found = bhu.find("a" );
-    if (found  != string::npos ){
-    cout<<"third word found  at :   ";
-    cout<<found<<endl;
-
-    }
-   found = bhu.find("What" );
-    if (found  != string::npos ){
-    cout<<"fourth word found at : ";
-    cout<<found<<endl;
-
-    }
-    found = bhu.find("wonderfull" );
-    if (found  != string::npos ){
-    cout<<"fifth word found  at:   ";
-    cout<<found<<endl;
-
-    }
     return 0;
 }
