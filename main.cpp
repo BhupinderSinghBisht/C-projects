20ea208a28528f48c8497220e6250b4d8d28770e
Bhuwan Bisht <37290289+BhupinderSinghBisht@users.noreply.github.com>
2018-03-13 00:14:06 +0530

    To find if a 3 and 4 digit number is Armstrong number or number

--- main.cpp
+++ main.cpp
@@ -1,15 +1,68 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
 int main()
 {
-     char a;
-     cout<<"please enter an alphabet you wanna find ascii value of :";
-     cin>>a;
+int a,h,d,d1=0;
+cout<<"enter your number :"<<endl;
+cin >>a;
+int i=a;
+if(a>=100 && a<=999)
+{
+cout<<"its a three digit"<<endl;
 
-     cout<<"ascii  will be :"<<(int)a;
+h = 3;
 
-
-    return 0;
 }
+
+if(a>=1000 && a<=10000)
+
+{ cout<<"its a four digit number "<<endl;
+
+h = 4;
+
+}
+
+for (int n=1 ; n<=h; n++)
+
+{
+int b=a%10;
+int d=pow(b,h);
+d1=d+d1;
+int c=a/10;
+a=c;
+
+}
+
+cout<<d1<<endl;
+
+if (d1==i) {
+
+cout<<"its a armstrong number : "<<i; }
+
+else {
+
+cout <<"it is not an armstrong number :"<<i; }
+
+/*
+
+int b=a%10;
+int c=a/10;
+int d=c%10;
+int e=c/10;
+
+int f = (eee)+(ddd)+(bbb);
+if (f==a)
+    {
+    cout<<"This is what we call armstrong number ";
+    }
+    else
+    {
+    cout<<"IT is not armstrong ";
+    } */
+
+return 0;
+
+}
+
