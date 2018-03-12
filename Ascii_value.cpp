858d0d729acd1617f8bd9402cfb4447aabfb8c9c
Bhuwan Bisht <37290289+BhupinderSinghBisht@users.noreply.github.com>
2018-03-13 00:26:09 +0530

    Ascii_value

---
+++ Ascii_value.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+ {
+     char a;
+     cout<<"enter an alpabet :";
+     cin>>a;
+     cout<<"Ascii Value will be :"<<(int)a;
+
+     return 0;
+ }
