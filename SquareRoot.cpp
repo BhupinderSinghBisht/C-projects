5abee1205ef66ee882d40abe62b5510e53c6df2d
Bhuwan Bisht <37290289+BhupinderSinghBisht@users.noreply.github.com>
2018-03-15 23:59:40 +0530

    To find square root of a number

---
+++ SquareRoot.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <math.h>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    float a,b ;
+
+    cout<<"Enter the value you want to find out :";
+
+    cin>>a;
+
+    b=sqrt(a);
+
+    cout<<setprecision(10)<<"squart root will be :"<<b;
+    return 0;
+}
