b542922145cbc5c6fbc17c12b5a86a5bbf7157b5
Bhuwan Bisht <37290289+BhupinderSinghBisht@users.noreply.github.com>
2018-03-15 23:13:13 +0530

    convert Celsius to Fahrenheit

---
+++ Celsius_to_Fahrenheit.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b;
+
+    cout<<"enter the temperature : ";
+    cin>>a;
+
+    b = a * 9/5 + 32;
+
+    cout<<"temperature in ferenhite will be :"<<b<<endl;
+
+    return 0;
+}
